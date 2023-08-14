6ba14fb1691504e60f0d30a588f6e0b71a0ec561
adhamahmed4 <adhamahmeds2312@gmail.com>
2023-08-15 00:58:31 +0300

    login page

---
+++ lib/Login/login.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+class MyLoginPage extends StatefulWidget {
+  @override
+  _MyLoginPageState createState() => _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<MyLoginPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool _passwordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFEEF1F5),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/login_bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 230,
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+                    child: Image.asset('assets/images/logo.png'),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Color(0xFF2B2E83),
+                          fontSize: 30,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Login to access your account.',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(255, 250, 250, 250),
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0xFFFFAB4A))),
+                          labelText: 'Username',
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: !_passwordVisible,
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(255, 250, 250, 250),
+                          filled: true,
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                            child: Icon(
+                              _passwordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              // Implement Forgot Password functionality
+                            },
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(color: Color(0xFF2B2E83)),
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: Size(120, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(24),
+                              ),
+                            ),
+                            onPressed: () {
+                              // Implement Login functionality
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Center(
+                        child: Container(
+                          height: 40,
+                          width: 280,
+                          decoration: BoxDecoration(
+                            color: Colors
+                                .white, // Set the background color to white
+                            border: Border.all(
+                              color: const Color.fromARGB(
+                                  255, 255, 255, 255), // Set the border color
+                            ),
+                            borderRadius: BorderRadius.circular(
+                                6), // Set the border radius
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Don't have an account?",
+                                  style: TextStyle(
+                                    color: Color(0xFF2B2E83),
+                                  )),
+                              SizedBox(width: 4),
+                              GestureDetector(
+                                onTap: () {
+                                  // Implement registration redirection
+                                },
+                                child: Text(
+                                  'Create',
+                                  style: TextStyle(
+                                    color: Colors.orange,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Icon(
+                                Icons.arrow_forward,
+                                color: Colors.orange,
+                                size: 20,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(height: 100), // Adding some space at the bottom
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
